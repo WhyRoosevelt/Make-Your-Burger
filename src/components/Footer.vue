515262d2e35c911e53438fb5cdae250a02f00fe0
Roosevelt <85763135+WhyRoosevelt@users.noreply.github.com>
2022-09-23 13:44:42 -0300

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer id="footer">
+    <p>Make Your Burguer &copy; 2022</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+ #footer {
+  height: 200px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #FCBA03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+ }
+</style>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="main-container">
+      <h1>Gerenciar pedidos:</h1>
+      <Dashboard />
+    </div>
+  </div>
+</template>
+
+<script>
+import Dashboard from '../components/Dashboard'
+export default {
+  name: 'Pedidos',
+  components: {
+    Dashboard
+  }
+}
+</script>
